7545640823ece976f6c2414efa618804e0149970
pro-dev-ryan <azizul.developer@gmail.com>
2022-10-25 15:47:14 +0600

    course page designed done

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,20 +4,26 @@ import { Link } from "react-router-dom";
 function Card() {
   // const {} = card;
   return (
-    <div className="cards mb-3">
-      <div
-        className="card-template glass backdrop-blur-sm bg-gradient-to-tl from-gray-800 to-transparent 
+    <div className="cards mt-3 lg:mx-3 lg:w-[100%]">
+      <div>
+        <div
+          className="card-template glass backdrop-blur-sm bg-sky-100 
       hover:shadow lg:shadow-stone-600 transition-shadow duration-500 lg:bg-opacity-50 z-50"
-      >
-        <img src="" className="w-[90%] lg:w-full" alt="" />
-        <div className="info">
-          <h4 className="blogQue mt-2  text-lg">{}</h4>
-          <p className="lg:mb-4 mb-3  text-center text-orange-300 font-button">
-            Total Quizes: {}
-          </p>
-          <Link to={``} className="cardBtn">
-            Take Test
-          </Link>
+        >
+          <img
+            src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"
+            className="w-[90%] lg:w-full"
+            alt=""
+          />
+          <div className="info">
+            <h4 className="blogQue mt-2 text-lg font-textHead">
+              {} Course Name
+            </h4>
+            <p className="lg:mb-4 mb-3 font-text">Duration: {}</p>
+            <Link to={``} className="cardBtn">
+              Take Test
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div>
-      <h4 className="text-3xl font-bold font-heading">This is sidebar</h4>
+      <h4 className="text-3xl tracking-wider w-fit border-b-2 border-stone-300 font-bold font-heading lg:ml-10">
+        Classes We Offer
+      </h4>
+      <ul className="flex flex-col items-center courses  lg:mt-6">
+        <Link>course-1</Link>
+        <Link>course-2</Link>
+        <Link>course-3</Link>
+        <Link>course-4</Link>
+      </ul>
     </div>
   );
 };
